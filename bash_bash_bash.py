b5218bcec9b35e38c99d7a370656c873463d47d4
Li Xuanji <xuanji.li@airbnb.com>
2016-07-14 13:57:37 -0700

    add bash_bash_bash

---
+++ bash_bash_bash.py
@@ -0,0 +1,47 @@
+# Record datadog metrics into a DB for easier SLA reporting
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+from datetime import datetime, timedelta
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2016, 6, 9, 1, 0),
+    'email': ['xuanji@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': False,
+    'retries': 3,
+    'retry_delay': timedelta(minutes=1),
+}
+
+dag = DAG('bash_bash_bash', default_args=default_args)
+
+# t1, t2 and t3 are examples of tasks created by instatiating operators
+t1 = BashOperator(
+    task_id='print_date',
+    bash_command='date',
+    dag=dag)
+
+t2 = BashOperator(
+    task_id='sleep',
+    bash_command='sleep 5',
+    retries=3,
+    dag=dag)
+
+templated_command = """
+    {% for i in range(5) %}
+        echo "{{ ds }}"
+        echo "{{ macros.ds_add(ds, 7)}}"
+        echo "{{ params.my_param }}"
+    {% endfor %}
+"""
+
+t3 = BashOperator(
+    task_id='templated',
+    bash_command=templated_command,
+    params={'my_param': 'Parameter I passed in'},
+    dag=dag)
+
+t2.set_upstream(t1)
+t3.set_upstream(t1)
